24bcae19e97f085884ab68b2f1b01ad67e977617
marie101010 <marie.behaegel@hotmail.fr>
2020-03-25 15:24:07 +0100

    Amélioration2

--- owner1.php
+++ owner1.php
@@ -7,30 +7,30 @@
 <link rel="stylesheet" href="hackaton.css">
 </head>
 <body>
-<!--Formulaire-->
-<form action="Controller" method="POST">
-    <fieldset>
-        <legend><a>Formulaire de validation de la transaction:</a></legend>
-        <!--Id numérique (entier 10 chiffres)-->
-        <div>
-            <label for="id"><a>Identifiant numérique:</a></label>
-            <input type="number" min="10" max="10" name="id" placeholder="Entrez 10 chiffres" required>
-        </div>
-            <!--Name-->
-        <div>
-            <label for="name"><a>Nom:</a></label>
-            <input type="text" name="id" placeholder="Entrez votre nom" required>
-        </div>
-        <!--Password-->
-        <div>
-            <label for="password"><a>Mot de passe:</a></label>
-            <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe" required>
-        </div>
-        <!--Submit-->
-        <div class="bouton">
-            <button type="submit"> Valider la transaction </button>
-        </div>
-    </fieldset>
-</form>
+    <!--Formulaire-->
+    <form action="Controller" method="POST">
+        <fieldset>
+            <legend><a>Formulaire de validation de la transaction:</a></legend>
+            <!--Id numérique (entier 10 chiffres)-->
+            <div>
+                <label for="id"><a>Identifiant numérique:</a></label>
+                <input type="number" min="10" max="10" name="id" placeholder="Entrez 10 chiffres" required>
+            </div>
+                <!--Name-->
+            <div>
+                <label for="name"><a>Nom:</a></label>
+                <input type="text" name="id" placeholder="Entrez votre nom" required>
+            </div>
+            <!--Password-->
+            <div>
+                <label for="password"><a>Mot de passe:</a></label>
+                <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe" required>
+            </div>
+            <!--Submit-->
+            <div class="bouton">
+                <button type="submit"> Valider la transaction </button>
+            </div>
+        </fieldset>
+    </form>
 </body>
 </html>
