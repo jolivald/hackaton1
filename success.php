891dee6b569937f9bce573abddde24ee0ea1c08d
lauhus <laurine.hussong@hotmail.fr>
2020-03-25 14:08:48 +0100

    utilisation snippet

--- success.php
+++ success.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head> 
-<meta charset="UTF-8">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validation aboutie</title>
 
 <!--Eventuelle CSS / Pourquoi ne pas rajouter animation sur H1 ?
-    
-    <style>
+
+<style>
 .titre
 {
     display:flex;
     justify-content:space-around;
     align-items:center;
 }
-.titre
+.titre2
 {
     display:flex;
     justify-content:space-around;
@@ -20,12 +22,13 @@
 }
 </style>
 -->
-
-    <title> Validation aboutie </title>
-
 </head>
+
 <body>
     <h1 class="titre"> Merci ! </h1>
-    <h3 class="titre2"> Vous avez tous réussi. </h3>
+    <h3 class="titre2"> Vous avez tous réussi. </h3> 
 </body>
 </html>
+
+
+
