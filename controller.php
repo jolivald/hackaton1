bf1585aca92969fb58d30cab2b970782e1feab56
clayroth <clayration@gmail.com>
2020-03-25 12:12:56 +0100

    Select BDD

--- controller.php
+++ controller.php
@@ -8,7 +8,7 @@ $mdp = '';
 
 try {
     $bdd = new PDO($dsn,$user,$mdp);
-    echo 'BDD connectée';
+    echo 'BDD connectée'. '<br>'.'<br>'.'<br>';
 } catch ( PDOException $E) {
     echo 'erreur de type = '.$E;
 }
@@ -16,8 +16,30 @@ try {
 // Reception Des Données GET
 
 $identifiant_num = $_GET['']; //Entre le nom attribué dans votre champs Form
-$name = $_GET['']; //Entre le nom attribué dans votre champs Form
+$name = $_GET['orlando']; //Entre le nom attribué dans votre champs Form
 $validation_key= $_GET['']; //Entre le nom attribué dans votre champs Form
+
+
+//nom en BDD
+
+$name ='name';
+$Val_Num = 'identifiant_num';
+$Val_key ='validation_key';
+
+// Controlle avec la BDD
+
+
+$rs_select = $bdd->query("SELECT * FROM user");
+
+
+
+while($d = $rs_select->fetch(PDO::FETCH_ASSOC))
+{
+    echo $d[$name].'<br>';
+    echo $d[$Val_Num].'<br>';
+    echo $d[$Val_key].'<br>';
+    echo '----------------- '.'<br>';
+}
 
 
 
