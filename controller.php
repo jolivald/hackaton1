aaf0a44b8c55baec6e6f9ac80a5799672489dc17
clayroth <clayration@gmail.com>
2020-03-25 15:34:54 +0100

    Maj Commentaire

--- controller.php
+++ controller.php
@@ -1,65 +1,67 @@
 <?php
 
-// CONNEXTION A LA BDD
+//--------------------------- CONNEXTION A LA BDD------------------------------------------
 
-$dsn = 'mysql:host=localhost;dbname=insertion_sql;charset=utf8';
-$user = 'alaji';
-$mdp = 'alaji';
+$dsn = 'mysql:host=localhost;dbname=insertion_sql;charset=utf8'; //entrer votre config BDD
+$user = 'alaj'; //entrer votre nom d'utilisateur
+$mdp = 'alaji'; //entrer votre mot de pass
 
 try {
     $bdd = new PDO($dsn,$user,$mdp);
     echo 'BDD connectée'. '<br>'.'<br>'.'<br>';
 } catch ( PDOException $E) {
     
-    die('erreur de type = '.$E);
+    die( header('Location: index.php'));
 }
 
-// Reception Des Données POST
+// -------------------------- Reception Des Données POST ------------------------------------
 
 $identifiant_num = $_POST['']; //Entre le nom attribué dans votre champs Form
 $ReqName = $_POST['']; //Entre le nom attribué dans votre champs Form
 $validation_key= $_POST['']; //Entre le nom attribué dans votre champs Form
 
 
-//nom en BDD
+//-----------------------nom des colonnes BDD------------------------------------
 
 $BDD_name ='name';
 $Val_Num = 'identifiant_num';
 $Val_key ='validation_key';
 
-// Controlle avec la BDD
+//---------------------------- Recuperation des données en BDD-------------------------------------
 
-
-$rs_select = $bdd->prepare("SELECT * FROM user WHERE name=?");
-$rs_select-> execute (array($ReqName));
+$rs_select = $bdd->prepare("SELECT * FROM user WHERE name=?"); // selection avec comme conditions le nom
+$rs_select-> execute (array($ReqName)); //Recuperation Grace au nom
 
  
     $d = $rs_select->fetch(PDO::FETCH_OBJ);
     
-        if($ReqName == $d->name)
+        if($ReqName == $d->name) //Verification si le nom existe
         {
             
 
-            if($identifiant_num == $d->$Val_Num && $validation_key == $d->$Val_key)
+            if($identifiant_num == $d->$Val_Num && $validation_key == $d->$Val_key) //Verification si la clé de validation et le numero d'identifiant sont correct
             {
-                
-                setcookie($d->id,$d->name, time()+ 3600 * 24 *365);
-                header('Location: index.php?IdValid=1');
+                //Les données sont correct :
+
+                setcookie($d->id,$d->name, time()+ 3600 * 24 *365); // création du cookie avec comme nom le ID et valeur le NAME
+                header('Location: index.php?IdValid=1'); // Renvoi vers index avec une validation sur 1 en GET
                 
             }
 
-            else
+            else 
             {
+                // les données sont incorrect :
+
+                header('Location: index.php?IdValid=0'); // Renvoi vers index avec une validation sur 0 en GET
                 
-                header('Location: index.php?IdValid=0');
             }
 
         }
         
         else
         {
-            
-            header('Location: index.php?IdValid=0');
+            // le name est inconnu
+            header('Location: index.php?IdValid=0'); // Renvoi vers index avec une validation sur 0 en GET
         }
         
     
