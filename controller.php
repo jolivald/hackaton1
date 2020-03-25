5e9d7f21539ab154dac75047e67d8f0206e3cfe0
clayroth <clayration@gmail.com>
2020-03-25 15:36:45 +0100

    majCom

--- controller.php
+++ controller.php
@@ -3,22 +3,22 @@
 //--------------------------- CONNEXTION A LA BDD------------------------------------------
 
 $dsn = 'mysql:host=localhost;dbname=insertion_sql;charset=utf8'; //entrer votre config BDD
-$user = 'alaji'; //entrer votre nom d'utilisateur
-$mdp = 'alaji'; //entrer votre mot de pass
+$user = 'alaji';    //entrer votre nom d'utilisateur
+$mdp = 'alaji';     //entrer votre mot de pass
 
 try {
     $bdd = new PDO($dsn,$user,$mdp);
     echo 'BDD connectée'. '<br>'.'<br>'.'<br>';
-} catch ( PDOException $E) {
-    
-    die( header('Location: index.php'));
-}
+    } catch ( PDOException $E) {
+        
+        die( header('Location: index.php'));
+    }
 
 // -------------------------- Reception Des Données POST ------------------------------------
 
-$identifiant_num = $_POST['']; //Entre le nom attribué dans votre champs Form
-$ReqName = $_POST['']; //Entre le nom attribué dans votre champs Form
-$validation_key= $_POST['']; //Entre le nom attribué dans votre champs Form
+$identifiant_num = $_POST[''];  //Entre le nom attribué dans votre champs Form
+$ReqName = $_POST[''];          //Entre le nom attribué dans votre champs Form
+$validation_key= $_POST[''];    //Entre le nom attribué dans votre champs Form
 
 
 //-----------------------nom des colonnes BDD------------------------------------
@@ -29,8 +29,8 @@ $Val_key ='validation_key';
 
 //---------------------------- Recuperation des données en BDD-------------------------------------
 
-$rs_select = $bdd->prepare("SELECT * FROM user WHERE name=?"); // selection avec comme conditions le nom
-$rs_select-> execute (array($ReqName)); //Recuperation Grace au nom
+$rs_select = $bdd->prepare("SELECT * FROM user WHERE name=?");  // selection avec comme conditions le nom
+$rs_select-> execute (array($ReqName));                         //Recuperation Grace au nom
 
  
     $d = $rs_select->fetch(PDO::FETCH_OBJ);
@@ -43,8 +43,8 @@ $rs_select-> execute (array($ReqName)); //Recuperation Grace au nom
             {
                 //Les données sont correct :
 
-                setcookie($d->id,$d->name, time()+ 3600 * 24 *365); // création du cookie avec comme nom le ID et valeur le NAME
-                header('Location: index.php?IdValid=1'); // Renvoi vers index avec une validation sur 1 en GET
+                setcookie($d->id,$d->name, time()+ 3600 * 24 *365);     // création du cookie avec comme nom le ID et valeur le NAME
+                header('Location: index.php?IdValid=1');                // Renvoi vers index avec une validation sur 1 en GET
                 
             }
 
