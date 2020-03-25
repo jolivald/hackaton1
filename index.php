492b68c81b9263ec4569f8d8faa8aa46a452bc56
gedovius <annegedovius@hotmail.fr>
2020-03-25 18:06:38 +0100

    ma bank

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<?php 
-if(isset ($_COOKIE[1]) && isset($_COOKIE[2])&& isset($_COOKIE[3])&& isset($_COOKIE[4]))
-{
-    header('Location: success.php');
-}
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="fr">
 
